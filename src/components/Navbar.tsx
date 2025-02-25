1c7c7edf9546244174c7f56683edd3388d88b0e4
Henryk <63795118+henior2@users.noreply.github.com>
2025-02-25 10:09:59 +0100

    Navbar stuff + better responsivity

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,9 @@ import { useEffect, useRef, useState } from 'preact/hooks'
 function Navbar(props: any) {
 	const root = useRef(document.querySelector(':root')!)
 
+	const [collapsed, setCollapsed] = useState(true)
+	const [themeLight, setThemeLight] = useState(false)
+
 	const toggleTheme = (updateSaved = false) => {
 		const rootElement = root.current
 		rootElement.classList.toggle('dark')
@@ -12,6 +15,8 @@ function Navbar(props: any) {
 		if (updateSaved) {
 			localStorage.setItem('theme', rootElement.classList.contains('dark') ? 'dark' : 'light')
 		}
+
+		setThemeLight(!themeLight)
 	}
 
 	useEffect(() => {
@@ -31,40 +36,41 @@ function Navbar(props: any) {
 
 		if (browserTheme == 'light') {
 			root.current.classList.remove('dark')
+			setThemeLight(true)
 		}
 	}, [])
 
-	const [collapsed, setCollapsed] = useState(true)
-
 	return (
-		<nav class="custom-responsive dark:bg-background-900 bg-text-50 sticky top-0 m-auto flex items-end gap-6 p-4 font-mono text-xl">
-			<a class="me-4 text-4xl font-bold" href="/">
-				henior
-			</a>
-			<div
-				class={
-					'dark:bg-background-900 bg-text-50 absolute top-full left-0 flex w-full flex-col items-center gap-6 pb-6 md:visible md:relative md:translate-0 md:flex-row md:items-end md:pb-0' +
-					(collapsed ? ' invisible' : '')
-				}
-			>
-				<a href="#about">About me</a>
-				<a href="#contact">Contact</a>
+		<nav class="dark:bg-background-900 bg-text-50 fixed top-0 w-full">
+			<div class="custom-responsive mx-auto flex items-end gap-6 p-4 font-mono text-xl">
+				<a class="me-4 text-4xl font-bold" href="/#">
+					henior
+				</a>
+				<div
+					class={
+						'dark:bg-background-900 bg-text-50 absolute top-full left-0 flex w-full flex-col items-center gap-6 pb-6 md:visible md:relative md:translate-0 md:flex-row md:items-end md:pb-0' +
+						(collapsed ? ' invisible' : '')
+					}
+				>
+					<a href="#about">About me</a>
+					<a href="#contact">Contact</a>
+				</div>
+				<div class="grow"></div>
+				<button
+					class="hover:text-primary cursor-pointer self-center text-2xl transition-colors duration-200"
+					type="button"
+					onClick={() => toggleTheme(true)}
+				>
+					{themeLight ? props.themeIconOn : props.themeIconOff}
+				</button>
+				<button
+					class="hover:text-primary cursor-pointer self-center text-2xl transition-colors duration-200 md:hidden"
+					type="button"
+					onClick={() => setCollapsed(!collapsed)}
+				>
+					{collapsed ? props.navbarIconOn : props.navbarIconOff}
+				</button>
 			</div>
-			<div class="grow"></div>
-			<button
-				class="text-primary cursor-pointer self-center text-2xl"
-				type="button"
-				onClick={() => toggleTheme(true)}
-			>
-				{props.themeIcon}
-			</button>
-			<button
-				class="text-primary cursor-pointer self-center text-2xl md:hidden"
-				type="button"
-				onClick={() => setCollapsed(!collapsed)}
-			>
-				{props.navbarIcon}
-			</button>
 		</nav>
 	)
 }
