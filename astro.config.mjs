be5bc02ed087f554d047104a7906b58d2394b076
Henryk <63795118+henior2@users.noreply.github.com>
2025-03-03 17:45:25 +0100

    Add astro-favicons
    Signed-off-by: Henryk <63795118+henior2@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,24 @@ import preact from '@astrojs/preact'
 
 import icon from 'astro-icon'
 
+import favicons from 'astro-favicons'
+import { readFile } from 'fs/promises'
+
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
 
-	integrations: [icon(), preact({ compat: true, devtools: true })],
+	integrations: [
+		icon(),
+		preact({ compat: true, devtools: true }),
+		favicons({
+			input: {
+				favicons: [await readFile('src/assets/hero.png')],
+			},
+			name: 'henior',
+			short_name: 'henior',
+		}),
+	],
 })
