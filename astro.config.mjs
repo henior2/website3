1cf20d568b5b2b25714d0159aa6a43a3984da6d0
Henryk <63795118+henior2@users.noreply.github.com>
2025-06-23 21:24:20 +0200

    Change indentation, update deps, switch from Preact to Vanilla JS
    Signed-off-by: Henryk <63795118+henior2@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
-import preact from '@astrojs/preact'
 
 import icon from 'astro-icon'
 
@@ -11,23 +10,22 @@ import { readFile } from 'fs/promises'
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
+  vite: {
+    plugins: [tailwindcss()],
+  },
 
-	integrations: [
-		icon(),
-		preact({ compat: true, devtools: true }),
-		favicons({
-			input: {
-				favicons: [await readFile('src/assets/hero.png')],
-			},
-			name: 'henior',
-			short_name: 'henior',
-			manifest: {
-				display_override: ['browser'],
-				start_url: '/',
-			}
-		}),
-	],
+  integrations: [
+    icon(),
+    favicons({
+      input: {
+        favicons: [await readFile('src/assets/hero.png')],
+      },
+      name: 'henior',
+      short_name: 'henior',
+      manifest: {
+        display_override: ['browser'],
+        start_url: '/',
+      },
+    }),
+  ],
 })
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,70 +1,68 @@
-import { useEffect, useState } from 'preact/hooks'
+// import { useEffect, useState } from 'preact/hooks'
 
-// warning: this is probably the worst React code you'll ever see in your life
+// function Navbar(props: any) {
+//   const [collapsed, setCollapsed] = useState(true)
+//   const [themeLight, setThemeLight] = useState(false)
 
-function Navbar(props: any) {
-	const [collapsed, setCollapsed] = useState(true)
-	const [themeLight, setThemeLight] = useState(false)
+//   const toggleTheme = () => {
+//     document.documentElement.classList.toggle('dark')
 
-	const toggleTheme = () => {
-		document.documentElement.classList.toggle('dark')
+//     localStorage.setItem('theme', !themeLight ? 'light' : 'dark')
 
-		localStorage.setItem('theme', !themeLight ? 'light' : 'dark')
+//     setThemeLight(!themeLight)
+//   }
 
-		setThemeLight(!themeLight)
-	}
+//   useEffect(() => {
+//     const savedTheme = localStorage.getItem('theme')
 
-	useEffect(() => {
-		const savedTheme = localStorage.getItem('theme')
+//     // dark mode looks better, light mode was added just for accessibility
+//     const browserTheme = savedTheme || 'dark'
 
-		// dark mode looks better, light mode was added just for accessibility
-		const browserTheme = savedTheme || 'dark'
+//     if (!savedTheme) {
+//       localStorage.setItem('theme', browserTheme)
+//     }
 
-		if (!savedTheme) {
-			localStorage.setItem('theme', browserTheme)
-		}
+//     if (browserTheme === 'light') {
+//       setThemeLight(true)
+//     }
+//   }, [])
 
-		if (browserTheme === 'light') {
-			setThemeLight(true)
-		}
-	}, [])
+//   return (
+//     <nav class="fixed top-0 w-full">
+//       <div class="dark:bg-background-900 bg-text-50 custom-responsive mx-auto flex items-end gap-6 p-4 font-mono text-xl">
+//         <a class="me-4 text-4xl font-bold" href="/#">
+//           henior
+//         </a>
+//         <div
+//           class={
+//             'dark:bg-background-900 bg-text-50 time absolute top-full left-0 -z-10 flex w-full flex-col items-center gap-6 pb-6 md:visible md:relative md:top-auto md:z-0 md:translate-0 md:flex-row md:items-end md:pb-0 md:transition-none' +
+//             (collapsed && ' invisible')
+//           }
+//         >
+//           <a class="hover:text-accent dark:hover:text-secondary" href="/#about">
+//             About me
+//           </a>
+//           <a class="hover:text-accent dark:hover:text-secondary" href="/#contact">
+//             Contact
+//           </a>
+//           <a class="hover:text-accent dark:hover:text-secondary" href="/#friends">
+//             Friends
+//           </a>
+//         </div>
+//         <div class="grow"></div>
+//         <button class="hover:text-primary cursor-pointer self-center text-2xl" type="button" onClick={toggleTheme}>
+//           {themeLight ? props.themeIconOn : props.themeIconOff}
+//         </button>
+//         <button
+//           class="hover:text-primary cursor-pointer self-center text-2xl md:hidden"
+//           type="button"
+//           onClick={() => setCollapsed(!collapsed)}
+//         >
+//           {collapsed ? props.navbarIconOn : props.navbarIconOff}
+//         </button>
+//       </div>
+//     </nav>
+//   )
+// }
 
-	return (
-		<nav class="fixed top-0 w-full">
-			<div class="dark:bg-background-900 bg-text-50 custom-responsive mx-auto flex items-end gap-6 p-4 font-mono text-xl">
-				<a class="me-4 text-4xl font-bold" href="/#">
-					henior
-				</a>
-				<div
-					class={
-						'dark:bg-background-900 bg-text-50 time absolute top-full left-0 -z-10 flex w-full flex-col items-center gap-6 pb-6 md:visible md:relative md:top-auto md:z-0 md:translate-0 md:flex-row md:items-end md:pb-0 md:transition-none' +
-						(collapsed && ' invisible')
-					}
-				>
-					<a class="hover:text-accent dark:hover:text-secondary" href="/#about">
-						About me
-					</a>
-					<a class="hover:text-accent dark:hover:text-secondary" href="/#contact">
-						Contact
-					</a>
-					<a class="hover:text-accent dark:hover:text-secondary" href="/#friends">
-						Friends
-					</a>
-				</div>
-				<div class="grow"></div>
-				<button class="hover:text-primary cursor-pointer self-center text-2xl" type="button" onClick={toggleTheme}>
-					{themeLight ? props.themeIconOn : props.themeIconOff}
-				</button>
-				<button
-					class="hover:text-primary cursor-pointer self-center text-2xl md:hidden"
-					type="button"
-					onClick={() => setCollapsed(!collapsed)}
-				>
-					{collapsed ? props.navbarIconOn : props.navbarIconOff}
-				</button>
-			</div>
-		</nav>
-	)
-}
-
-export default Navbar
+// export default Navbar
